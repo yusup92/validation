7a76d3240b727b20f715aa10d5e50cd7531d9456
yusupov <yusup.92@mail.ru>
2018-05-16 23:12:16 +0500

    success fayly uytgetdim

--- success.php
+++ success.php
@@ -8,50 +8,55 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $input = json_decode($inpjson, true);
         $errors = [];
 
-        if (isset($input['gender'])) {
-            if ($input['gender'] == 'NULL') {
-                $errors[] = "gender is requeired";
-            }
+        if (empty($input["gender"])) {
+        $errors['error'] = "gender is required";
         }
+        elseif ($input["gender"] !=='Male' && $input["gender"] !=='Female') {
+            $errors['error'] = "Please select Male or Female";
+        }
+      
 
         if (empty($input["fname"])) {
-            $errors[] = "firstname is required";
+            $errors['error'] = "fname is required";
         }
 
         if (empty($input["lname"])) {
-            $errors[] = "lastname is required";
+            $errors['error'] = "lname is required";
         }
 
         if (empty($input["email"])) {
-            $errors[] = "email is required";
-        } else {
-            // regex match happen here
+            $errors['error'] = "email is required";
+        } elseif {
+
+            $email = $input["email"];
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+             $errors['error'] = "Invalid email format"; 
+                }
+            
         }
 
         if (empty($input["phonenumber"])) {
-            $errors[] = "phonenumber is required";
+            $errors['error'] = "phonenumber is required";
         }
 
+       
         if (count($errors) !== 0) {
-            throw new Exception(json_encode($errors), 500);
+            throw new Exception(json_encode($errors), 500); //throw new Exception(#code, 500)
         } else {
-            return "Validation is success";
+            echo "Validation is success";
         }
 
-    } catch (Exception $e) {
-        header('HTTP/1.1 500 Internal Server Booboo');
-        header('Content-Type: application/json; charset=UTF-8');
+    } catch (Exception $e) {                                        //
+        header('HTTP/1.1 500 Internal Server Booboo');              //
+        header('Content-Type: application/json; charset=UTF-8');    //
 
-        die(json_encode(array(
-            'error' => array(
-                'msg' => json_decode($e->getMessage()),
-                'code' => $e->getCode(),
+        die(json_encode(array(                                      //
+            'error' => array(                                       //
+                'msg' => json_decode($e->getMessage()),             //
+                'code' => $e->getCode(),                            //
             ),
         )));
     }
 }
 
-/*  header('Content-Type: application/json; charset=UTF-8');
-echo json_encode([
-"kidfsdf" => "hello world"]); */
 ?>
