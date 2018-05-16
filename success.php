39a23d65d901c5807701cb0283dec5d3cb1cf3e0
berdimyradov <kesha_yashok@mail.ru>
2018-05-17 00:02:40 +0500

    :bug: :recycle: Fixed validation errors; HTTP-405 error handling is added

--- success.php
+++ success.php
@@ -8,55 +8,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $input = json_decode($inpjson, true);
         $errors = [];
 
-        if (empty($input["gender"])) {
-        $errors['error'] = "gender is required";
+        $gender = $input["gender"];
+        if (empty($gender)) {
+            $errors['gender'] = "gender is required";
+        } elseif ($gender !== 'Male' || $gender !== 'Female') {
+            $errors["gender"] = "Please select Male or Female";
         }
-        elseif ($input["gender"] !=='Male' && $input["gender"] !=='Female') {
-            $errors['error'] = "Please select Male or Female";
-        }
-      
 
         if (empty($input["fname"])) {
-            $errors['error'] = "fname is required";
+            $errors["fname"] = "fname is required";
         }
 
         if (empty($input["lname"])) {
-            $errors['error'] = "lname is required";
+            $errors["lname"] = "lname is required";
         }
 
+        $email = $input["email"];
         if (empty($input["email"])) {
-            $errors['error'] = "email is required";
-        } else {
-
-            $email = $input["email"];
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-             $errors['error'] = "Invalid email format"; 
-                }
-            
+            $errors["email"] = "email is required";
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors["email"] = "Invalid email format";
         }
 
         if (empty($input["phonenumber"])) {
-            $errors['error'] = "phonenumber is required";
+            $errors["phonenumber"] = "phonenumber is required";
         }
 
-       
         if (count($errors) !== 0) {
-            throw new Exception(json_encode($errors), 500); //throw new Exception(#code, 500)
+            throw new Exception(json_encode($errors), 500);
         } else {
             echo "Validation is success";
         }
 
-    } catch (Exception $e) {                                        //
-        header('HTTP/1.1 500 Internal Server Booboo');              //
-        header('Content-Type: application/json; charset=UTF-8');    //
+    } catch (Exception $e) {
+        header('HTTP/1.1 500 Internal Server Error');
+        header('Content-Type: application/json; charset=UTF-8');
 
-        die(json_encode(array(                                      //
-            'error' => array(                                       //
-                'msg' => json_decode($e->getMessage()),             //
-                'code' => $e->getCode(),                            //
-            ),
+        die(json_encode(array(
+            'errors' => $errors,
+            'code' => $e->getCode(),
         )));
     }
+} else {
+    header("HTTP/1.0 405 Method Not Allowed");
+    exit();
 }
 
 ?>
