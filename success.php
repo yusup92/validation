df788c99ee9bae70801f4a416255d3f85db3b12e
yusupov <yusup.92@mail.ru>
2018-05-17 09:48:27 +0500

    success uytgetdim yene

--- success.php
+++ success.php
@@ -8,32 +8,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $input = json_decode($inpjson, true);
         $errors = [];
 
-        $gender = $input["gender"];
+        $gender = trim($input["gender"]);
         if (empty($gender)) {
             $errors['gender'] = "gender is required";
-        } elseif ($gender !== 'Male' || $gender !== 'Female') {
+        } elseif ($gender !== 'Male' && $gender !== 'Female') {
             $errors["gender"] = "Please select Male or Female";
         }
 
+
+        $fname=trim($input["fname"]);
         if (empty($input["fname"])) {
             $errors["fname"] = "fname is required";
+        }elseif (!preg_match("/^[a-zA-Z'-]/", $input['fname'])) {
+            $errors["fname"] = "fname is not valid";
+
         }
 
+
+        $lname=trim($input["lname"]);
         if (empty($input["lname"])) {
             $errors["lname"] = "lname is required";
+        }elseif (!preg_match("/^[a-zA-Z'-]/", $input['lname'])) {
+            $errors["lname"] = "lname is not valid";
+
         }
 
-        $email = $input["email"];
+        $email = trim($input["email"]);
         if (empty($input["email"])) {
             $errors["email"] = "email is required";
         } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $errors["email"] = "Invalid email format";
         }
 
-        if (empty($input["phonenumber"])) {
+        $phonenumber = trim($input["phonenumber"]);
+                if (empty($input["phonenumber"])) {
             $errors["phonenumber"] = "phonenumber is required";
-        }
+        } 
+        elseif(!preg_match("/^[0-9]{3}-[0-9]{4}-[0-9]{4}$/", $phonenumber)) {
 
+
+            $errors["phonenumber"] = "the number format must be 000-0000-0000";
+
+}
         if (count($errors) !== 0) {
             throw new Exception(json_encode($errors), 500);
         } else {
